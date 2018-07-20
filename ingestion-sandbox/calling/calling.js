a531ac292a7146f4f973d5808799a9de82a9d411
dromomaniac <evanlhendrix@gmail.com>
2018-07-20 20:03:16 +0200

    refactor multiple axios calls into one async function, still not working

--- ingestion-sandbox/calling/calling.js
+++ ingestion-sandbox/calling/calling.js
@@ -9,13 +9,14 @@ const queryString = require('query-string');
 
 // ingest data with axios 
 const authURL = 'http://192.168.32.124:6600/api/Account/Token?api_key=horizon';
+const facilitiesURL = 'http://192.168.32.124:6600/api/horizon/facilities';
 
-const facilitiesURL = 'http://192.168.32.124:6600/api/horizon/facilities'
-// TODO: put these in env variables
 const creds = { 'username': process.env.GPM_USERNAME, 'password': process.env.GPM_PASSWORD }
-let userToke;
+
+let accessToken;
 let authStr;
 const facilityIdArray = []; 
+const invertersByFacilityArray = [];
 
 const getToken = async () => {
   try {
@@ -27,41 +28,57 @@ const getToken = async () => {
 }
 
 const getFacilityIds = async () => {
+  try {
   const response = await getToken();
   // console.log('\n\n\nresponse = ', response)
   // console.log('response.data.AccessToken ' + response.data.AccessToken);
-    userToke = response.data.AccessToken;
-  // console.log('userToke = ', userToke);
-    authStr = 'Bearer '.concat(userToke);
-    // console.log('authStr = ', authStr)
-  try {
-    return await axios.get( facilitiesURL, { headers: { Authorization: authStr } });
+  accessToken = response.data.AccessToken;
+  // console.log('accessToken = ', accessToken);
+  authStr = 'Bearer '.concat(accessToken);
+  // console.log('authStr = ', authStr)
+  const facilityIdsResponse = await axios( facilitiesURL, { headers: { Authorization: authStr } });
+  
+  if (facilityIdsResponse.data) {
+    console.log(`Got ${Object.entries(facilityIdsResponse.data).length} facilities`)
+  }
+  facilityIdsResponse.data.forEach( facility => {
+    const facilityIdLocal = (facility.Parameters[0]) ? 
+    facilityIdArray.push(facility.Parameters[0].Key.FacilityId) : null;
+    return facilityIdLocal;
+  });
+  console.log('facilityIdArray = ', facilityIdArray);
+  console.log(facilityIdArray.length, "facilities with a facility id")
+  
+  const promises = facilityIdArray.map( async facility => {
+    const devicesByTypeInverterURL = `http://192.168.32.124:6600/api/horizon/facilities/${facility}/devices/by-type/INVERTER`;
+      const response = await axios( 
+        devicesByTypeInverterURL, 
+        { headers: { Authorization: authStr }}
+      );
+
+    } );
+    return {
+      FacilityId: response.data.FacilityId,
+      Type: response.data.Type,
+      Id: response.data.Id,
+      InvertersInPlantArray: response.data.Parameters
+    }
+
+
+    // wait until all promises resolve
+    const results = await Promise.all(promises)
+    // use the results
+    console.log('inverters in each plant? = ', results)
+
+
   } catch (error) {
     console.error(error)
   }
 }
 
-const countFacilities = async () => {
-  const response = await getFacilityIds();
-  console.log('\n\n FROM countFacilities!');
-  // console.log('\n\n response.data = ', JSON.stringify(response.data, null, 2));
-  // console.log('response.data = ', response.data );
-  if (response.data) {
-    console.log(`Got ${Object.entries(response.data).length} facilities`)
-  }
-  response.data.forEach( facility => {
-    return (facility.Parameters[0]) ? 
-    facilityIdArray.push(facility.Parameters[0].Key.FacilityId) : null;
-/*     if (facility.Parameters[0] ) {
-      return facilityIdArray.push(facility.Parameters[0]);
-    } */
-  });
-  console.log('facilityIdArray = ', facilityIdArray);
-  console.log(facilityIdArray.length, "facilities with a facility id")
-}
 
 // getFacilityIds();
-countFacilities()
+getFacilityIds()
 
 /* 
  axios.post(authURL, queryString.stringify(creds))
