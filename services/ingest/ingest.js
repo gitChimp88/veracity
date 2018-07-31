e9a83a024e51959786036b2be20640e9e937b1b1
dromomaniac <evanlhendrix@gmail.com>
2018-07-31 12:56:40 +0200

    SAVEPOINT

--- services/ingest/ingest.js
+++ services/ingest/ingest.js
@@ -6,11 +6,7 @@ const to = require('await-to-js').default;
 const axios = require('axios');
 var async = require("async");
 
-/* axios.interceptors.response.use(function (response) {
-	return response;
-}, function (error) {
-	return Promise.reject(error);
-}); */
+
 require('dotenv').config({path:'/Users/evanhendrix1/programming/code/green-power-monitor/experiment-instatrust/veracity-app/services/.env'});
 
 console.log('you. are. AWESOME!');  
@@ -23,88 +19,45 @@ const queryString = require('query-string');
 const getInverterVariableNames = async () => {
 	try {
 		const facilitiesURL = 'http://192.168.32.124:6600/api/horizon/facilities';
+		const variableIdURL = 'http://192.168.32.124:6600/api/horizon/parametertovariable/deviceparameter';
 		const creds = { 'username': process.env.GPM_USERNAME, 'password': process.env.GPM_PASSWORD }
-		let accessToken;
-		let authStr;
 		const facilityIdArray = []; 
-		const inverterParamsArray = [];
-		let bearerConfig = { headers: { 'Authorization': authStr } };
+		let authStr; 	
+		// a test for gitlens 2	
+	  const authString = await getBearerString(creds);
+		console.log('authString = ', authString)
+		let bearerConfig = { headers: { 'Authorization': authString } }
 
-		const getTokenPromise = await getToken(creds);
-		// console.log('\n\n\ngetTokenPromise = ', getTokenPromise)
-		console.log('getTokenPromise.data.AccessToken = ', getTokenPromise.data.AccessToken);
-		accessToken = await getTokenPromise.data.AccessToken;
-		// console.log('accessToken = ', accessToken);
-		authStr = 'Bearer '.concat(await accessToken);
-		// console.log('authStr = ', authStr)
-		const facilityIdsResponse = await axios( facilitiesURL, { headers: { Authorization: authStr } });
+		// ********   1. get facility data	
+		const facilityIdsResponse = await axios( facilitiesURL, { headers: { Authorization: authString} });
 		
 		// make array of facility ids
 		facilityIdsResponse.data.forEach( facility => {
 			if (facility && facility.Parameters[0]) {
 			facilityIdArray.push(facility.Parameters[0].Key.FacilityId) ;
 		}});
-		// console.log('facilityIdArray = ', facilityIdArray);
-		// console.log(facilityIdArray.length, "facilities with a facility id")
 	
-		// get inverters for each facility
+		// ********   2.  Get inverter information for each facility
 		const promises = facilityIdArray.map( async facility => {
 			const devicesByTypeInverterURL = `http://192.168.32.124:6600/api/horizon/facilities/${facility}/devices/by-type/INVERTER`;
-			const response = await axios( devicesByTypeInverterURL, { headers: { Authorization: authStr } } );
-			/* Making dynamic key names: 
-				return {
-				[`invertersForFacility${facility}`]: response.dat // should be array of inverters
-			} */
+			const response = await axios( devicesByTypeInverterURL, { headers: { 'Authorization': authString} } );
 			if (response.data) return response.data // array of inverters
 		});
-		// response.data && response.data.Parameters && response.data.Parameters.length > 0
 
-	// wait until all promises resolve
+		// wait until all promises resolve
 		// array of arrays. each child array is a list of inverters for a facility  
 		const invertersArrayNotFlat = await Promise.all(promises)
-		// console.log('inverters in each plant? = \n ', JSON.stringify(invertersArrayNotFlat, null, 2));
-		// console.log('inverters for each plant = \n ', invertersArrayNotFlat);
-		
-		// flatten array
 		let invertersArrayAndZeros = [].concat.apply([],invertersArrayNotFlat);
-		// console.log('invertersArrayAndZeros = ', invertersArrayAndZeros);
 
-		// for each inverter (object) in Array: return Parameters.Key  (which has
-		// DeviceId and Parameter) now we have one array of objects we could reduce
-		// like this:
-		// https://stackoverflow.com/questions/34398279/map-and-filter-an-array-at-the-same-time-in-javascript
-		//
-/*     const parAndDevIds = invertersArray.reduce( facility => {
-			if ( facility.Parameters[0] !== undefined ) { 
-				let dataToKeep =  {
-					FacilityId: facility.FacilityId,
-					DeviceId: facility.Parameters[0].Key.DeviceId,
-					ParameterId: facility.Parameters[0].Key.ParameterId,
-				} 
-
-			} 
-		},[]); */
-
-		// console.log('parAndDevIds = ',
-		// JSON.stringify(parAndDevIds, null, 2));
-		// console.log('parAndDevIds = ', parAndDevIds);
-		// console.log('invertersArrayFlat = ,', invertersArray)
-
-	/* map over array of facilities and for each facility: 
-			const newFacility = {}
-			map over all parameters in facility.Parameters array, doing: 
-				save each property to a key in newObject      
-
-	*/
 		// filter out facilitites that don't have inverters in them
 		const invertersArrayFiltered = invertersArrayAndZeros.filter( inverter =>  {
 			// console.log( 'inverter.Parameters.length = ', inverter.Parameters.length);
 			return inverter.Parameters.length > 0
 		});
-		console.log('invertersArrayFiltered = ', invertersArrayFiltered)
+		// console.log('invertersArrayFiltered = ', invertersArrayFiltered)
 
 		const invertersArray = invertersArrayFiltered.map((inverter, indexO) => { 
-			console.log('\n\n**************\n inverter: ', inverter, 'indexO: ', indexO);
+			// console.log('\n\n**************\n inverter: ', inverter, 'indexO: ', indexO);
 			let tempObj = {}
 			// filter this inverter.Parameters array, return resulting object
 			let newObj = inverter.Parameters.filter( param => param.Name == 'Energy');
@@ -121,9 +74,7 @@ const getInverterVariableNames = async () => {
 			 return tempObj;
 			});
 
-		callForVariables(invertersArray, authStr)
-
-	
+		callForVariables(invertersArray, authString, variableIdURL)
 
 	} catch (error) {
 			if (error.response) {
@@ -145,50 +96,57 @@ const getInverterVariableNames = async () => {
 		console.error('\n\n\n console.error = \n',error)
 	}
 }
-async function getToken (credsParam) {
-	const authURL = 'http://192.168.32.124:6600/api/Account/Token?api_key=horizon';
-	console.log('creds = ', credsParam)
-	try {
-		console.log('credsParam = ', credsParam)
-		return await axios.post( authURL, credsParam);
 
-	} catch (error) {
-		console.error(error)
-	}
+
+
+// const getDeviceVariables = callForVariables(x, y, z)
+
+// const getDeviceParametersForIrradiance = callForVariables(x, y, z)
+
+async function getBearerString (credsParam) {
+	const authURL = 'http://192.168.32.124:6600/api/Account/Token?api_key=horizon';
+	// console.log('creds = ', credsParam)
+	let getTokenPromise = {}
+	try {
+		// console.log('credsParam = ', credsParam)
+		 getTokenPromise = await axios.post( authURL, credsParam);
+		}	catch (error) {
+			console.error(error)
+		}
+	// console.log( 'bearer sting = ', 'Bearer '.concat(getTokenPromise.data.AccessToken));
+	return 'Bearer '.concat(getTokenPromise.data.AccessToken);
+
 };
 
+/* const getTokenPromise = await getToken(creds);
+		// console.log('\n\n\ngetTokenPromise = ', getTokenPromise)
+		console.log('getTokenPromise.data.AccessToken = ', getTokenPromise.data.AccessToken);
+		accessToken = await getTokenPromise.data.AccessToken;
+		// console.log('accessToken = ', accessToken);
+		authString= 'Bearer '.concat(await accessToken);
+ */
+
+
+
 // takes array of promises and returns array of variables
- function callForVariables(arr, authStr) {
-	console.log( 'Array of promises input to callForVariables = ', arr);
+ function callForVariables(arr, authString, varUrlParam) {
+	console.log( 'Array input to callForVariables = ', arr);
 	const variableIdURL = 'http://192.168.32.124:6600/api/horizon/parametertovariable/deviceparameter';
 	let requestData = {};
 	const variableIdPromises = arr.map( async inverter => {
 		try { 
-			console.log('\n\n\n\n\n');
-			console.log('inverter = ', JSON.stringify( inverter , null, 2));
-				requestData = {
+			requestData = {
 				'DeviceId':  inverter.DeviceId,
 				'ParameterId': inverter.ParameterId
 			}
-
-			// let requestObj = {
-			// 	url: variableIdURL,  
-			// 	data: requestData,
-			// 	headers: { 'Authorization': authStr } 
-      // }
-
-			// console.log('object passed to axios = ', JSON.stringify( requestObj, null, 2 ))
-      // console.log('requestData = ', JSON.stringify(requestData, null, 2)); 
-      
-			const variableIdResponse = await axios.post(variableIdURL, requestData, { headers: { Authorization: authStr } });
-			console.log('variableIdResponse.data = ', variableIdResponse.data)
-/*       // add returned variable to object, then return
-			let retObj = inverter;
-			retObj.variableName = variableIdResponse.data;
-			if (variableIdResponse.data) return retObj;
-			 */
-			console.log( 'response = ', variableIdResponse)
-			if (variableIdResponse.data) return variableIdResponse.data;
+			const variableIdResponse = await axios.post(variableIdURL, requestData,  { headers: { 'Authorization': authString }});
+				/*       // add returned variable to object, then return
+				let retObj = inverter;
+				retObj.variableName = variableIdResponse.data;
+				if (variableIdResponse.data) return retObj;
+				*/
+				// console.log( 'response = ', variableIdResponse)
+				if (variableIdResponse.data) return variableIdResponse.data;
 			
 		} catch (error) {
 			if (error.response) {
@@ -206,12 +164,11 @@ async function getToken (credsParam) {
 				// Something happened in setting up the request that triggered an Error
 				console.log('Error in setting up request....', error.message);
 			}
-			console.log('error.config = \n', error.config);
-	}
+			// console.log('error.config = \n', error.config);
+		}
 	});
 	
-	
- return Promise.all(variableIdPromises)
+		return Promise.all(variableIdPromises)
 			.then((values) => {
 				console.log('all loaded YO', values)
 				return JSON.stringify(values);
@@ -220,25 +177,6 @@ async function getToken (credsParam) {
 			}); 
 
 	}
-	
-	// utility funcitons
- function some( promises, count = 1 ){
- 
- const wrapped = promises.map( promise => promise.then(value => ({ success: true, value }), () => ({ success: false })) );
-	return Promise.all( wrapped ).then(function(results){
-			const successful = results.filter(result => result.success);
-			 if( successful.length < count )
-						throw new Error("Only " + successful.length + " resolved.")
-						return successful.map(result => result.value);
-	});
- }
-
-
-/* { 
-		userId: '1',
-		title: 'examplestring',
-		completed: false
-	} */
 	
  
  getInverterVariableNames();
