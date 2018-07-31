db08d5934b46407734204596ed3e2419ae6d7071
dromomaniac <evanlhendrix@gmail.com>
2018-07-31 16:56:14 +0200

    SAVEPOINT

--- services/ingest/ingest.js
+++ services/ingest/ingest.js
@@ -16,172 +16,180 @@ const queryString = require('query-string');
 
 
 
-const getInverterVariableNames = async () => {
-	try {
-		const facilitiesURL = 'http://192.168.32.124:6600/api/horizon/facilities';
-		const variableIdURL = 'http://192.168.32.124:6600/api/horizon/parametertovariable/deviceparameter';
-		const creds = { 'username': process.env.GPM_USERNAME, 'password': process.env.GPM_PASSWORD }
-		const facilityIdArray = []; 
-		let authStr; 	
-		// a test for gitlens 2	
-	  const authString = await getBearerString(creds);
-		console.log('authString = ', authString)
-		let bearerConfig = { headers: { 'Authorization': authString } }
+const getEnergy = async (pOi) => {
+  try {
+    const facilitiesURL = 'http://192.168.32.124:6600/api/horizon/facilities';
+    const variableIdURL = 'http://192.168.32.124:6600/api/horizon/parametertovariable/deviceparameter';
+    const creds = { 'username': process.env.GPM_USERNAME, 'password': process.env.GPM_PASSWORD }
+    const facilityIdArray = []; 
 
-		// ********   1. get facility data	
-		const facilityIdsResponse = await axios( facilitiesURL, { headers: { Authorization: authString} });
-		
-		// make array of facility ids
-		facilityIdsResponse.data.forEach( facility => {
-			if (facility && facility.Parameters[0]) {
-			facilityIdArray.push(facility.Parameters[0].Key.FacilityId) ;
-		}});
-	
-		// ********   2.  Get inverter information for each facility
-		const promises = facilityIdArray.map( async facility => {
-			const devicesByTypeInverterURL = `http://192.168.32.124:6600/api/horizon/facilities/${facility}/devices/by-type/INVERTER`;
-			const response = await axios( devicesByTypeInverterURL, { headers: { 'Authorization': authString} } );
-			if (response.data) return response.data // array of inverters
-		});
+    // a test for gitlens 2	
+    const authString = await getBearerString(creds);
+    console.log('authString = ', authString)
+    // let bearerConfig = { headers: { 'Authorization': authString } }
 
-		// wait until all promises resolve
-		// array of arrays. each child array is a list of inverters for a facility  
-		const invertersArrayNotFlat = await Promise.all(promises)
-		let invertersArrayAndZeros = [].concat.apply([],invertersArrayNotFlat);
+    // ********   1. get facility data	
+    const facilityIdsResponse = await axios( facilitiesURL, { headers: { Authorization: authString} });
+    
+    // make array of facility ids
+    facilityIdsResponse.data.forEach( facility => {
+      if (facility && facility.Parameters[0]) {
+      facilityIdArray.push(facility.Parameters[0].Key.FacilityId) ;
+    }});
+  
+    // ********   2.  Get inverter information for each facility
+    const promises = facilityIdArray.map( async facility => {
+      const devicesByTypeInverterURL = `http://192.168.32.124:6600/api/horizon/facilities/${facility}/devices/by-type/INVERTER`;
+      const response = await axios( devicesByTypeInverterURL, { headers: { 'Authorization': authString} } );
+      if (response.data) return response.data // array of inverters
+    });
 
-		// filter out facilitites that don't have inverters in them
-		const invertersArrayFiltered = invertersArrayAndZeros.filter( inverter =>  {
-			// console.log( 'inverter.Parameters.length = ', inverter.Parameters.length);
-			return inverter.Parameters.length > 0
-		});
-		// console.log('invertersArrayFiltered = ', invertersArrayFiltered)
+    // wait until all promises resolve
+    // array of arrays. each child array is a list of inverters for a facility  
+    const invertersArrayNotFlat = await Promise.all(promises)
+    let invertersArrayAndZeros = [].concat.apply([],invertersArrayNotFlat);
 
-		const invertersArray = invertersArrayFiltered.map((inverter, indexO) => { 
-			// console.log('\n\n**************\n inverter: ', inverter, 'indexO: ', indexO);
-			let tempObj = {}
-			// filter this inverter.Parameters array, return resulting object
-			let newObj = inverter.Parameters.filter( param => param.Name == 'Energy');
-				// add preperties you need from the inverter level, then return to map
-				// the whole object; this will build the 'invertersArray'
-				tempObj.DeviceId =  newObj[0].Key.DeviceId;
-				tempObj.ParameterId =  newObj[0].Key.ParameterId;
-				tempObj.Name =  newObj[0].Name;
-				tempObj.ParameterSubType =  newObj[0].ParameterSubType;
-				tempObj.ParameterType =  newObj[0].Insolation;
-				tempObj.Units =  newObj[0].Units;
-				tempObj.FacilityId = inverter.FacilityId;
-				tempObj.Id = inverter.Id;
-			 return tempObj;
-			});
+    // filter out facilitites that don't have inverters in them
+    const invertersArrayFiltered = invertersArrayAndZeros.filter( inverter =>  {
+      // console.log( 'inverter.Parameters.length = ', inverter.Parameters.length);
+      return inverter.Parameters.length > 0
+    });
+    // console.log('invertersArrayFiltered = ', invertersArrayFiltered)
 
-		callForVariables(invertersArray, authString, variableIdURL)
+    const invertersArray = invertersArrayFiltered.map((inverter, indexO) => { 
+      // console.log('\n\n**************\n inverter: ', inverter, 'indexO: ', indexO);
+      let tempObj = {}
+      // filter this inverter.Parameters array, return resulting object
+      let newObj = inverter.Parameters.filter( param => param.Name == 'Energy');
+        // add preperties you need from the inverter level, then return to map
+        // the whole object; this will build the 'invertersArray'
+        tempObj.DeviceId =  newObj[0].Key.DeviceId;
+        tempObj.ParameterId =  newObj[0].Key.ParameterId;
+        tempObj.Name =  newObj[0].Name;
+        tempObj.ParameterSubType =  newObj[0].ParameterSubType;
+        tempObj.ParameterType =  newObj[0].Insolation;
+        tempObj.Units =  newObj[0].Units;
+        tempObj.FacilityId = inverter.FacilityId;
+        tempObj.Id = inverter.Id;
+       return tempObj;
+      });
 
-	} catch (error) {
-			if (error.response) {
-				// The request was made and the server responded with a status code
-				// that falls out of the range of 2xx
-				console.log('\nError, request made, but server responded with ...', error.response.data);
-				console.log('\nError.response.status = ', error.response.status);
-				console.log('\nError.response.headers = ', error.response.headers);
-			} else if (error.request) {
-				// The request was made but no response was received `error.request` is
-				// an instance of XMLHttpRequest in the browser and an instance of
-				// http.ClientRequest in node.js
-				console.log('Error. Request made but no response received....', error.request);
-			} else {
-				// Something happened in setting up the request that triggered an Error
-				console.log('Error in setting up request....', error.message);
-			}
-			console.log('error.config = \n', error.config);
-		console.error('\n\n\n console.error = \n',error)
-	}
+    if (pOi === 'inverter') {
+      callForVariables(invertersArray, authString, variableIdURL, pOi)
+    } else if (pOi === 'plant' ) {
+      callForVariables(plantArray, authString, variableIdURL, pOi)
+    } else {
+      console.log('input plant or inverter as string') 
+      return;
+    }
+
+  } catch (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log('\nError, request made, but server responded with ...', error.response.data);
+        console.log('\nError.response.status = ', error.response.status);
+        console.log('\nError.response.headers = ', error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received `error.request` is
+        // an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log('Error. Request made but no response received....', error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error in setting up request....', error.message);
+      }
+      console.log('error.config = \n', error.config);
+    console.error('\n\n\n console.error = \n',error)
+  }
 }
 
 
 
-// const getDeviceVariables = callForVariables(x, y, z)
-
-// const getDeviceParametersForIrradiance = callForVariables(x, y, z)
 
 async function getBearerString (credsParam) {
-	const authURL = 'http://192.168.32.124:6600/api/Account/Token?api_key=horizon';
-	// console.log('creds = ', credsParam)
-	let getTokenPromise = {}
-	try {
-		// console.log('credsParam = ', credsParam)
-		 getTokenPromise = await axios.post( authURL, credsParam);
-		}	catch (error) {
-			console.error(error)
-		}
-	// console.log( 'bearer sting = ', 'Bearer '.concat(getTokenPromise.data.AccessToken));
-	return 'Bearer '.concat(getTokenPromise.data.AccessToken);
-
+  const authURL = 'http://192.168.32.124:6600/api/Account/Token?api_key=horizon';
+  // console.log('creds = ', credsParam)
+  let getTokenPromise = {}
+  try {
+    // console.log('credsParam = ', credsParam)
+    getTokenPromise = await axios.post( authURL, credsParam);
+  }	catch (error) {
+    console.error(error)
+  }
+  // console.log( 'bearer sting = ', 'Bearer '.concat(getTokenPromise.data.AccessToken));
+  return 'Bearer '.concat(getTokenPromise.data.AccessToken);
+  
 };
 
-/* const getTokenPromise = await getToken(creds);
-		// console.log('\n\n\ngetTokenPromise = ', getTokenPromise)
-		console.log('getTokenPromise.data.AccessToken = ', getTokenPromise.data.AccessToken);
-		accessToken = await getTokenPromise.data.AccessToken;
-		// console.log('accessToken = ', accessToken);
-		authString= 'Bearer '.concat(await accessToken);
- */
+// function getDeviceVariables = callForVariables(x, y, z)
 
+// function getDeviceParametersForIrradiance callForVariables(x, y, z)
 
+// takes array of object with parameters and info for elements, and returns array of variables
+ function callForVariables(arr, authString, varUrlParam, pOi) {
+  console.log( 'Array input to callForVariables = ', arr);
+  const variableIdURL = 'http://192.168.32.124:6600/api/horizon/parametertovariable/deviceparameter';
+  let requestData = {};
+  const variableIdPromises = arr.map( async inverter => {
+    try { 
+      requestData = {
+        'DeviceId':  inverter.DeviceId,
+        'ParameterId': inverter.ParameterId
+      }
+      const variableIdResponse = await axios({
+        method: 'post',
+        url: variableIdURL,
+        data: requestData,  
+        headers: { 'Authorization': authString }
+      });
+        /*       // add returned variable to object, then return
+        let retObj = inverter;
+        retObj.variableName = variableIdResponse.data;
+        if (variableIdResponse.data) return retObj;
+        */
+        console.log( 'response = ', variableIdResponse)
+        if (variableIdResponse.data) return variableIdResponse.data;
+      
+    } catch (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log('\nError, request made, but server responded with ...', error.response.data);
+        console.log('\nError.response.status = ', error.response.status);
+        console.log('\nError.response.headers = ', error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received `error.request` is
+        // an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log('Error. Request made but no response recieved....', error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error in setting up request....', error.message);
+      }
+      // console.log('error.config = \n', error.config);
+    }
+  });
+  
+    return Promise.all(variableIdPromises)
+      .then((rawValues) => {
+        console.log('rawValues', rawValues)
+        let values = rawValues.filter(val => val)	
+                              .map( filVal => ( {
+                                  FacilityId: filVal.Key.FacilityId,
+                                  DeviceId: filVal.Key.DeviceId,
+                                  VariableId: filVal.Key.VariableId,
+                                  Name: filVal.Name,
+                                  Unit: filVal.Unit
+                                } )
+                              );	
+        console.log(`The Variable ids for ${pOi} = `, values)
+        return values;
+      }, function() {
+        console.log('stuff failed')
+      }); 
 
-// takes array of promises and returns array of variables
- function callForVariables(arr, authString, varUrlParam) {
-	console.log( 'Array input to callForVariables = ', arr);
-	const variableIdURL = 'http://192.168.32.124:6600/api/horizon/parametertovariable/deviceparameter';
-	let requestData = {};
-	const variableIdPromises = arr.map( async inverter => {
-		try { 
-			requestData = {
-				'DeviceId':  inverter.DeviceId,
-				'ParameterId': inverter.ParameterId
-			}
-			const variableIdResponse = await axios({
-				method: 'post',
-				url: variableIdURL,
-				data: requestData,  
-				headers: { 'Authorization': authString }
-			});
-				/*       // add returned variable to object, then return
-				let retObj = inverter;
-				retObj.variableName = variableIdResponse.data;
-				if (variableIdResponse.data) return retObj;
-				*/
-				// console.log( 'response = ', variableIdResponse)
-				if (variableIdResponse.data) return variableIdResponse.data;
-			
-		} catch (error) {
-			if (error.response) {
-				// The request was made and the server responded with a status code
-				// that falls out of the range of 2xx
-				console.log('\nError, request made, but server responded with ...', error.response.data);
-				console.log('\nError.response.status = ', error.response.status);
-				console.log('\nError.response.headers = ', error.response.headers);
-			} else if (error.request) {
-				// The request was made but no response was received `error.request` is
-				// an instance of XMLHttpRequest in the browser and an instance of
-				// http.ClientRequest in node.js
-				console.log('Error. Request made but no response recieved....', error.request);
-			} else {
-				// Something happened in setting up the request that triggered an Error
-				console.log('Error in setting up request....', error.message);
-			}
-			// console.log('error.config = \n', error.config);
-		}
-	});
-	
-		return Promise.all(variableIdPromises)
-			.then((values) => {
-				console.log('all loaded YO', values)
-				return JSON.stringify(values);
-			}, function() {
-				console.log('stuff failed')
-			}); 
-
-	}
-	
+  }
+  
  
- getInverterVariableNames();
+ getEnergy('inverter');
